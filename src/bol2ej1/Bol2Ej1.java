ba8e8474771e4e47d88e79f809b68cde818d4dc8
dsoutofernandez <dsoutofernandez@eniac05.danielcastelao.org>
2014-10-07 09:07:46 +0200

    Area de un cuadrado (v1.1)

--- src/bol2ej1/Bol2Ej1.java
+++ src/bol2ej1/Bol2Ej1.java
@@ -6,12 +6,12 @@ import java.util.Scanner;
 public class Bol2Ej1 {
 
     public static void main(String[] args) {
-
-        System.out.println("Introduzca la base: ");
-        Scanner base = new Scanner (System.in);
         
         int lado = 0;
         int area = 0;
+        
+        System.out.println("Introduzca la base: ");
+        Scanner base = new Scanner (System.in);
         
         lado = base.nextInt();
         
